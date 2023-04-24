da6f7f640d50f97ab5edd816b496360097260814
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-04-24 17:26:20 +0200

    In HalfEdge, rename face -> triangle.
    Plus a toString() method.

--- src/main/java/org/mastodon/mesh/HalfEdge.java
+++ src/main/java/org/mastodon/mesh/HalfEdge.java
@@ -29,7 +29,7 @@ public class HalfEdge extends AbstractEdge< HalfEdge, Vertex, HalfEdgePool, Byte
 		return pool.getObjectIfExists( id, ref );
 	}
 
-	public Triangle face( final Triangle ref )
+	public Triangle triangle( final Triangle ref )
 	{
 		final int id = pool.face.get( this );
 		return pool.facePool.getObjectIfExists( id, ref );
@@ -46,6 +46,13 @@ public class HalfEdge extends AbstractEdge< HalfEdge, Vertex, HalfEdgePool, Byte
 			pool.twin.set( this, twin.getInternalPoolIndex() );
 			pool.twin.set( twin, getInternalPoolIndex() );
 		}
+	}
 
+	@Override
+	public String toString()
+	{
+		return String.format( "E%d (V%d -> V%d, NE%d, PE%d, TE%d, F%d)",
+				getInternalPoolIndex(), getSourceVertexInternalPoolIndex(), getTargetVertexInternalPoolIndex(),
+				pool.next.get( this ), pool.previous.get( this ), pool.twin.get( this ), pool.face.get( this ) );
 	}
 }
